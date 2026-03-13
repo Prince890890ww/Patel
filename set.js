55f1672531b261b03690522f74d10bcb2438d936
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2026-03-13 20:28:33 +0530

    Update set.js

--- set.js
+++ set.js
@@ -9,7 +9,7 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'IK~r9YRkI7Q#KDhwrAFvIaAi1wJZuAvt_bC7lVwtkxLjS8zvRtNU8t0',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "Malakz Emmy",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
